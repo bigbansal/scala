9db6dc7e902cafec9e1bfbea2e73d35bdb7f242b
bigbansal <bigbansal111@gmail.com>
2016-12-24 14:30:55 +1100

    Create SingletonPattern.scala

---
+++ designpatterns/SingletonPattern.scala
@@ -0,0 +1,7 @@
+/*
+An object defines a singleton object in Scala. There is only instance of this object in
+any given program and it can be passed around. You can define methods inside, variables inside this Object scala class.
+*/
+
+object SingletonPattern {
+}
